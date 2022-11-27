1e44e81228f36a6b67f6e1470eaf9d92e9bf3ea2
thedd-admin <dgrimesbusiness@gmail.com>
2022-11-27 18:31:15 -0500

    Update gulp with new packages
    Add in autoprefixer and cssnano for gulp

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,20 +1,41 @@
 const { src, dest } = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
 
 /* Variables */
 sassSrc = 'sass/**/*.scss';
 sassDest = '../assets/css/';
 
 
+
 function generateCSS(cb) {
+    var plugins = [
+        autoprefixer(),
+        cssnano(),
+    ];
     src(sassSrc)
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(rename({
         extname: ".css"
     }))
+    .pipe(postcss(plugins))
+    .pipe(sourcemaps.write('.'))
     .pipe(dest(sassDest));
     cb();
 }
 
 exports.css = generateCSS;
+
+// Use the following syntax to create a default task if needed (only need to use gulp in terminal)
+/*
+exports.default = () => (
+	gulp.src('src')
+		.pipe()
+		.pipe(gulp.dest('dist'))
+);
+*/
